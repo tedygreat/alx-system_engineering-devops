882b8ef236d49c0367ac1225b3fd64ee2e6be026
root <tedygreat@gmail.com>
2023-08-09 15:43:16 +0000

    task 1

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,4 +1,4 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 """
 a recursive function that queries the Reddit API and returns a list containing
 the titles of all hot articles for a given subreddit. If no results are found
